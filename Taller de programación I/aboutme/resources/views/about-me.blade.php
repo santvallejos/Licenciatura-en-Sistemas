28553d491797c6ef12ac2f4c09b6b5e920d6efd2
Santiago Vallejos <vallejossantiago1412@gmail.com>
2026-04-13 19:43:20 -0300

    Actividad N°2 de Taller de programación I

--- Taller de programación I/aboutme/resources/views/about-me.blade.php
+++ Taller de programación I/aboutme/resources/views/about-me.blade.php
@@ -4,32 +4,16 @@
 <head>
     <title>Sobre mí</title>
 
-    <style>
-        body {
-            font-family: Arial;
-            background-color: #f4f4f4;
-            margin: 40px;
-        }
-
-        .container {
-            background: white;
-            padding: 20px;
-            border-radius: 10px;
-            max-width: 600px;
-        }
-
-        h1 {
-            color: #333;
-        }
-
-        p {
-            line-height: 1.6;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/estilos.css">
 
 </head>
 
 <body>
+
+    <nav class="navbar">
+        <a href="/">Inicio</a>
+        <a href="/sobre-mi">Sobre mí</a>
+    </nav>
 
     <div class="container">
 
